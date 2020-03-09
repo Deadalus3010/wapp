ecf32d1441ac33ca5bfd8c903a52ca410a1dcb43
Deadalus3010 <deadalus3010@gmail.com>
2020-03-09 15:16:55 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,81 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';#
-const { HomeScreen, SettingsScreen } = require('../src/javascript/homescreen.js');
+import * as React from 'react';
+import { View } from 'react-native';
+import { BottomNavigation, Text } from 'react-native-paper';
+import FontAwesome, { SolidIcons, RegularIcons, BrandIcons } from 'react-native-fontawesome';
 
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Settings: SettingsScreen,
-});
+const { HomeScreen }      =   require('./src/script/screens/homescreen.js');
+const { NewsScreen }      =   require('./src/script/screens/newsscreen.js');
+const { ProductsScreen }  =   require('./src/script/screens/productsscreen'); 
+const { SettingsScreen }  =   require('./src/script/screens/settingsscreen');
 
-export default createAppContainer(TabNavigator);
+
+  /*****************  
+  #
+  #     [Variablen][Screen]
+  #
+  #     Hier werden Inhalte für die Hauptmenüs aus den dafür zuständigen Dateien geholt 
+  #
+  *****************/ 
+
+const HomeRoute = ()      => <View><HomeScreen/></View>;
+
+const NewsRoute = ()      => <View><NewsScreen/></View>;
+
+const ProductsRoute = ()  => <View><ProductsScreen/></View>;
+
+const SettingsRoute = ()  => <View><SettingsScreen/></View>;
+
+  /*****************  
+  #
+  #     [Nav]
+  #
+  #     Setzt Elemente auf den Bildschirm     
+  #
+  *****************/ 
+
+export default class Waermi extends React.Component {
+  state = {
+    index: 0,
+    routes: [                                             
+      { key: 'home',      title: 'Home',            icon: <FontAwesome style={{fontSize: 32}} icon={BrandIcons.github}></FontAwesome>,    color: '#3F51B5' },
+      { key: 'news',      title: 'News',            icon: 'album',          color: '#009688' },
+      { key: 'products',  title: 'Produkte',        icon: 'history',        color: '#795548' },
+      { key: 'settings',  title: 'Einstellungen',   icon: 'shopping-cart',  color: '#607D8B' },
+    ]
+  }
+
+  _handleIndexChange = index => this.setState({ index });
+
+  /*****************  
+  #
+  #     [Screen]
+  #
+  #     Definiert die Bottomnavbar    
+  #
+  *****************/ 
+
+  _renderScene = BottomNavigation.SceneMap({
+    home:     HomeRoute,
+    news:     NewsRoute,
+    products: ProductsRoute,
+    settings: SettingsRoute,
+  });
+
+  /*****************  
+  #
+  #     [Screen]
+  #
+  #     Rendert die Bottomnavbar    
+  #
+  *****************/ 
+
+  render() {
+    return (
+      <BottomNavigation
+        navigationState={this.state}
+        onIndexChange={this._handleIndexChange}
+        renderScene={this._renderScene}
+      />
+    );
+  }
+}
