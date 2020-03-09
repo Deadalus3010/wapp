9290b65f3a5aae5635abb009ba4b16f4f78c352c
Deadalus3010 <deadalus3010@gmail.com>
2020-03-09 15:11:11 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-
+import { createBottomTabNavigator } from 'react-navigation-tabs';#
 const { HomeScreen, SettingsScreen } = require('../src/javascript/homescreen.js');
 
 const TabNavigator = createBottomTabNavigator({
