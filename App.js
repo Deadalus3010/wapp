3acbd00c53c242abf8df50274841ccd9cf47c3f3
Nicolas Krieg <57000262+nicolaskrieg@users.noreply.github.com>
2020-11-25 17:51:11 +0100

    Hübschere BottomNavBar
    Einbau der alten, Icons.
    Farbschema ist nun Gelb, Schwarz, Grau.
    verschiedene Icons zwischen aktive und inaktiver Tabs
    Anpassung der Versionsbezeichnung auf 3.1

--- App.js
+++ App.js
@@ -2,8 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
  
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
  
 const { NewsScreen }      =   require('./src/script/screens/newsscreen.js');
 const { EventScreen }     =   require('./src/script/screens/eventscreen.js');
@@ -19,33 +18,60 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator
         tabBarPosition='bottom'
+        navigationOptions={{tabBarLabel: 'News'.toLowerCase(),}}
         tabBarOptions={{
-          activeTintColor: 'grey', 
+          activeTintColor: 'black', 
           style: {backgroundColor: 'yellow'},
-          inactiveTintColor: 'black', 
-          inactiveBackgroundColor: 'black',
+          inactiveTintColor: 'grey', 
           showIcon: 'true', 
-          labelStyle: {fontSize: 7},
+          tabStyle:{alignItems:'center'},
+          labelStyle: {fontSize: 11, 
+            margin: 0, 
+            padding: 0, 
+            textTransform: 'none' /*ohne dies sind alle Buchstaben groß */ 
+          },
+          inactiveBackgroundColor: 'black',
+          activeBackgroundColor: 'black',
+          indicatorStyle:{backgroundColor: 'black'},
+          upperCaseLabel: false,
           }}
         screenOptions={({route}) => ({
           tabBarIcon: ({focused, color, size}) => {
             let iconName;
             let iconcolor_ind;
             let iconcolor_def = 'black';
- 
+            
             if (route.name === 'News') {
               iconName = focused
-                  ? 'rebel'
-                  : 'tree';
+                  ? 'alpha-n-box'
+                  : 'alpha-n-box-outline';
               iconcolor_ind = 'yellow';
             }
             else if (route.name === 'Events') {
               iconName = focused 
-                  ? 'ra'
-                  : 'ra';
+                  ? 'alpha-e-box'
+                  : 'alpha-e-box-outline';
               iconcolor_ind = 'blue';
             }
-            return <Icon name={iconName} size={20} color={iconcolor_def} />;
+            else if (route.name === 'Produkte') {
+              iconName = focused 
+                  ? 'alpha-p-box'
+                  : 'alpha-p-box-outline';
+              iconcolor_ind = 'blue';
+            }
+            else if (route.name === 'Mitglieder') {
+              iconName = focused 
+                  ? 'alpha-m-box'
+                  : 'alpha-m-box-outline';
+              iconcolor_ind = 'blue';
+            }
+            else if (route.name === 'Kontakt') {
+              iconName = focused 
+                  ? 'alpha-k-box'
+                  : 'alpha-k-box-outline';
+              iconcolor_ind = 'blue';
+            }
+            return <Icon name={iconName} size={22} color={iconcolor_def} />;
           },
         })
         }>
